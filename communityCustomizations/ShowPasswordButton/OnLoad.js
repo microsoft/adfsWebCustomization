5af59400cdcdf8ed93491bf5d3f91b1b1f564597
Kirill Bogdanov <kirbogd@microsoft.com>
2018-04-13 12:17:39 +0300

    Initial ShowPasswordButton project

---
+++ communityCustomizations/ShowPasswordButton/OnLoad.js
@@ -0,0 +1,15 @@
+var inputArea = document.getElementById("inputArea");
+
+if(inputArea){
+     console.log("hello");
+     var showButton = document.createElement("div");
+ showButton.id = "showButton";  
+ showButton.innerHTML = "Show password";
+ inputArea.appendChild(showButton);
+ 
+ var appendedButton = document.getElementById("showButton");
+ appendedButton.onmousedown = function(){ document.getElementById("password").type = "text"; };
+   
+ appendedButton.onmouseup = function(){ document.getElementById("password").type = "password";};
+ 
+}
