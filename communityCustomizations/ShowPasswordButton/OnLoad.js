d199ccf920bc1eeb67f7578efb0e39b9129cd3f5
Matt Bongiovi <mattbo@microsoft.com>
2018-04-16 15:32:10 -0700

    Update docs to fix some typos (#26)

--- communityCustomizations/ShowPasswordButton/OnLoad.js
+++ communityCustomizations/ShowPasswordButton/OnLoad.js
@@ -1,15 +1,14 @@
 var inputArea = document.getElementById("inputArea");
+var showButton = document.getElementById("showButton");
 
-if(inputArea){
- console.log("hello");
- var showButton = document.createElement("div");
- showButton.id = "showButton";  
- showButton.innerHTML = "Show password";
- inputArea.appendChild(showButton);
- 
- var appendedButton = document.getElementById("showButton");
- appendedButton.onmousedown = function(){ document.getElementById("password").type = "text"; };
-   
- appendedButton.onmouseup = function(){ document.getElementById("password").type = "password";};
- 
+if ( inputArea && !showButton )
+{
+    var showButton = document.createElement("div");
+    showButton.id = "showButton";  
+    showButton.innerHTML = "Show password";
+    inputArea.appendChild(showButton);
+
+    var appendedButton = document.getElementById("showButton");
+    appendedButton.onmousedown = function(){ document.getElementById("password").type = "text"; };
+    appendedButton.onmouseup = function(){ document.getElementById("password").type = "password";};
 }
