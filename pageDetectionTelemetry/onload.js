d292f6963a110571b01aa69b3fe97fb10a534c98
Miles Gratz <milesgratz@gmail.com>
2019-10-21 15:08:46 -0500

    Update onload.js
    Define default endpointUrl and add information about custom endpoints

--- pageDetectionTelemetry/onload.js
+++ pageDetectionTelemetry/onload.js
@@ -66,7 +66,8 @@ var TelemetryManager = {
             function r(config) { t[config] = function () { var i = arguments; t.queue.push(function () { t[config].apply(t, i) }) } } var t = { config: config }, u = document, e = window, o = "script", s = u.createElement(o), i, f; for (s.src = config.url || "//az416426.vo.msecnd.net/scripts/a/ai.0.js", u.getElementsByTagName(o)[0].parentNode.appendChild(s), t.cookie = u.cookie, t.queue = [], i = ["Event", "Exception", "Metric", "PageView", "Trace"]; i.length;) r("track" + i.pop()); return r("setAuthenticatedUserContext"), r("clearAuthenticatedUserContext"), config.disableExceptionTracking || (i = "onerror", r("_" + i), f = e[i], e[i] = function (config, r, u, e, o) { var s = f && f(config, r, u, e, o); return s !== !0 && t["_" + i](config, r, u, e, o), s }), t
         }({
             samplingPercentage: 100,
-            instrumentationKey: "YOUR-KEY-HERE"
+            instrumentationKey: "YOUR-KEY-HERE",
+            endpointUrl: "https://dc.services.visualstudio.com/v2/track"   // modify endpoint for Azure Government or Azure China as per https://docs.microsoft.com/en-us/azure/azure-monitor/app/custom-endpoints
         });
     },
 
